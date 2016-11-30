d99ea8b609d290f2ba4d2d47d2546334fa016405
Alfonso Vásquez <avasquez@rivetlogic.com>
2016-11-30 14:41:50 -0600

    Initial Maven project files

---
+++ src/main/java/org/craftercms/DeployerApplication.java
@@ -0,0 +1,13 @@
+package org.craftercms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployerApplication.class, args);
+	}
+
+}

d931bd55a271f636ba0d38bc7ef222c26c4f96b8
Alfonso Vasquez <alfonso.vasquez@craftersoftware.com>
2018-04-30 17:03:27 -0400

    Code quality fixes:
    * Removed unused imports.

--- src/main/java/org/craftercms/deployer/utils/GitUtils.java
+++ src/main/java/org/craftercms/deployer/utils/GitUtils.java
@@ -16,21 +16,22 @@
  */
 package org.craftercms.deployer.utils;
 
-import org.apache.commons.lang3.StringUtils;
-import org.craftercms.commons.git.auth.GitAuthenticationConfigurator;
-import org.eclipse.jgit.api.*;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.StoredConfig;
-import org.eclipse.jgit.merge.MergeStrategy;
-import org.eclipse.jgit.transport.FetchResult;
-import org.eclipse.jgit.transport.PushResult;
-import org.eclipse.jgit.transport.RefSpec;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
+
+import org.apache.commons.lang3.StringUtils;
+import org.craftercms.commons.git.auth.GitAuthenticationConfigurator;
+import org.eclipse.jgit.api.CloneCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.PullCommand;
+import org.eclipse.jgit.api.PullResult;
+import org.eclipse.jgit.api.PushCommand;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.StoredConfig;
+import org.eclipse.jgit.merge.MergeStrategy;
+import org.eclipse.jgit.transport.PushResult;
 
 /**
  * Utility methods for Git operations.

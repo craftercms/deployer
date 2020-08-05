6ecae689cca9cbb267ee9b64440e9aec09b0499d
Jose Ross <jdross17@gmail.com>
2020-08-05 16:00:15 -0600

    Upgrade Manager: - Remove exclusion of static-assets for clear cache events

--- src/main/java/org/craftercms/deployer/impl/upgrade/operations/AbstractTargetUpgradeOperation.java
+++ src/main/java/org/craftercms/deployer/impl/upgrade/operations/AbstractTargetUpgradeOperation.java
@@ -41,6 +41,7 @@ public abstract class AbstractTargetUpgradeOperation extends AbstractUpgradeOper
 
     public static final String CONFIG_KEY_PROCESSOR = "processor";
     public static final String CONFIG_KEY_REPLACE = "replace";
+    public static final String CONFIG_KEY_REMOVE = "remove";
     public static final String CONFIG_KEY_PROPERTY = "property";
     public static final String CONFIG_KEY_PATTERN = "pattern";
     public static final String CONFIG_KEY_EXPRESSION = "expression";

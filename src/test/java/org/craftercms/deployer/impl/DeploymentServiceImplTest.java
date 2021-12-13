481b60224d23229dc48332813cae68f1a36618b2
Sumer Jabri <sumer.jabri@craftercms.org>
2021-12-13 10:32:44 -0500

    Updated Mockito's imports.

--- src/test/java/org/craftercms/deployer/impl/DeploymentServiceImplTest.java
+++ src/test/java/org/craftercms/deployer/impl/DeploymentServiceImplTest.java
@@ -27,7 +27,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Matchers.eq;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;

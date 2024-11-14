170bea005d7102416172d763f64c09bf56127389
Phil Nguyen <nquangphuong@gmail.com>
2024-11-15 00:41:52 +0700

    Add mode to deployer output (#473)

--- src/main/java/org/craftercms/deployer/impl/processors/FileOutputProcessor.java
+++ src/main/java/org/craftercms/deployer/impl/processors/FileOutputProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2022 Crafter Software Corporation. All Rights Reserved.
+ * Copyright (C) 2007-2024 Crafter Software Corporation. All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as published by
@@ -100,6 +100,7 @@ public class FileOutputProcessor extends AbstractPostDeploymentProcessor {
     protected void appendDeployment(CSVPrinter printer, Deployment deployment) throws IOException {
         ChangeSet changeSet = deployment.getChangeSet();
         printer.printRecord(
+                deployment.getMode(),
                 deployment.getStatus(),
                 deployment.getDuration(),
                 deployment.getStart().toInstant(),
